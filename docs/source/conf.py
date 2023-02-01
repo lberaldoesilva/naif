632a653e60d5a736aca5ad2adf753d0c117f3c90
Leandro Beraldo e Silva <lberaldoesilva@gmail.com>
2023-01-31 21:56:28 -0800

    updating version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'naif'
 copyright = '2023, Leandro Beraldo e Silva'
 author = 'Leandro Beraldo e Silva'
-release = '0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
